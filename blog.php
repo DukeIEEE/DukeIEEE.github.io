9f1eb9299fc84d04f21fa9f642b5b11bb93615dd
Jerry Pan <jerry.qy.pan@gmail.com>
2017-02-23 19:00:36 -0500

    Merged Media with Events

--- blog.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('simple_html_dom.php');
-$html = file_get_html('http://sites.duke.edu/ieee/');
-$ret = $html->find('div[id=main]');
-$img = $ret->find('img'); 
-$r->srcset = null;
-echo $ret[0];
-?>

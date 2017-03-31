4728816a4fa2e42eb84343d4f6c0e056e7aa5b58
Jerry Pan <qp7@duke.edu>
2017-03-31 01:37:32 -0400

    Updated Logo

--- js/creative.js
+++ js/creative.js
@@ -30,7 +30,7 @@
     // Fit Text Plugin for Main Header
     $("h1").fitText(
         1.2, {
-            minFontSize: '35px',
+            minFontSize: '30px',
             maxFontSize: '65px'
         }
     );
